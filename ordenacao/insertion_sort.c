e4fa8d08f9a7de1ce1025d8e602d0af1a3691755
Romeu Antunes <e795750@mail.caixa>
2019-09-02 16:33:18 -0300

    Arrumando algoritimo e comentando para estudo futuro

--- ordenacao/insertion_sort.c
+++ ordenacao/insertion_sort.c
@@ -1,48 +1,44 @@
 // INSERTION SORT
-// ESTÁVEL NÃO ALTERA A ORDEM DE DADOS
+// CARTA DE BARALHO NA MÃO;
 
-// cartas de baralho na mão
-// MELHOR CASO : O(N)
-// PIOR CASO: O(N²)
+// MELHOR CASO : O(N);
+// PIOR CASO: O(N²);
+// Algoritmo estável onde a ordem de dois elementros iguais não é alterada;
 
 #include <string.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void insere_array(int array[]){
-    int valor_aux;
-    int i=0;
-    while(scanf("%d", &valor_aux) != EOF){
-        array[i] = valor_aux;
-        i++;
+// Por padrão a função do insertion sort vai receber o vetor que vai ordenar e o tamanho do vetor;
+int insertion_sort(int vetor[], int tamanho_vetor){
+    // Variáveis auxiliares;
+    // Variável "i" responsável por escolher o número que iremos ordenar dentro do vetor;
+    // Variável "j" responsável por percorrer o vetor para verificar a posição de inserção;
+    // Variável "auxiliar" reponsável por realizar o swap quando encontrar a posição adequada;
+    // Ordenação em ordem crescente
+    int i=0, j=0, auxiliar; 
+    for(i=1; i < tamanho_vetor; i++){
+        j = i;
+        while( j > 0 && vetor[j] < vetor[j-1]){
+            auxiliar = vetor[j];
+            vetor[j] = vetor[j-1];
+            vetor[j-1] = auxiliar;
+            j--;
+        }
     }
-
-    insertion_sort(array,i);
 }
 
-void insertion_sort(int *ordenado, int tamanho){
-    int valor_atual, valor_compara, aux_realoca;
-    for(valor_atual = 1; valor_atual < tamanho; valor_atual++){
-        aux_realoca = ordenado[valor_atual];
-        for(valor_compara = valor_atual; (valor_compara > 0) && aux_realoca < ordenado[valor_compara-1]; valor_compara--){
-            ordenado[valor_compara] = ordenado[valor_compara -1];
-        }
-        ordenado[valor_compara] = aux_realoca;
-    }
-
-    for(int k=0; k<tamanho; k++){
-        if(k== tamanho -1){
-        printf("%d\n", ordenado[k]);
-        } else {
-        printf("%d ", ordenado[k]);
-        }
-
+void print_array(int vetor[], int tamanho_vetor){
+    for(int i=0; i<tamanho_vetor; i++){
+        printf("%d ", vetor[i]);
     }
 }
 
 int main(){
-    int numeros[1000];
-    insere_array(numeros);
+
+    int array_desordenado[15] = {-8,10,56,814,587,317,-57,9,27,86,1,89,20,35,67} ;
+    insertion_sort(array_desordenado,15);
+    print_array(array_desordenado,15);
     return 0;
 }
