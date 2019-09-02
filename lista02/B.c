943f24154926615077b6ddd6968d924da7b2c7f0
Romeu Antunes <romeucarvalho2009@hotmail.com>
2019-09-02 11:11:30 -0300

    Inicio da lista02 de EDA2

---
+++ lista02/B.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+// TLE ;-(
+// TODO
+//  INSERIR OS VALORES ORDENADOS;
+
+int main(){
+
+    int quantidade_numeros;
+
+    scanf("%d", &quantidade_numeros);
+
+    int array_proibido[quantidade_numeros];
+    int numero_proibido;
+
+    for(int i=0; i<quantidade_numeros; i++){
+        scanf("%d", &numero_proibido);
+        array_proibido[i] = numero_proibido;
+    } 
+
+    int numero_teste;
+    int immprime=0;
+
+    while(scanf("%d", &numero_teste) != EOF){
+        for(int j=0; j<quantidade_numeros; j++){
+            if(numero_teste == array_proibido[j]){
+                printf("sim\n");
+                immprime = 1;
+                break;
+            } else {
+                immprime = 2;
+            }
+        
+        }
+        if(immprime == 2){
+            printf("nao\n");
+
+        }
+    }
+
+
+    return 0;
+}
+
---
+++ lista02/C.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+// Questão da chamada, receber a quantidade de alunos, o numero sorteado e os nomes...
+// Ordenar os nomes só até a posição ou se posivel ordenar apenas aquela posição, pois assim garante a velocidade no código e exatidão na resposta;
+// Utilizar quick sort, passando como pivô o nome que voce deseja 
+// TODO
+
+
+int main(){
+
+    return 0;
+}
+
