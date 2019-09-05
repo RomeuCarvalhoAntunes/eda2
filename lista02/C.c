fb0852a130e061dbc961e251c722e0c9b8b52ab2
Romeu Antunes <e795750@mail.caixa>
2019-09-05 14:47:29 -0300

    Exercicio 3 da lista 02 de EDA-2

--- lista02/C.c
+++ lista02/C.c
@@ -3,13 +3,41 @@
 #include <stdio.h>
 #include <string.h>
 
-// Questão da chamada, receber a quantidade de alunos, o numero sorteado e os nomes...
-// Ordenar os nomes só até a posição ou se posivel ordenar apenas aquela posição, pois assim garante a velocidade no código e exatidão na resposta;
-// Utilizar quick sort, passando como pivô o nome que voce deseja 
-// TODO
+static int compara_strings(const void* nome_1, const void* nome_2){
+    return strcmp(*(const char**)nome_1, *(const char**)nome_2);
+}
+
+void ordena_nome(char* nomes[], int tamanho){
+    qsort(nomes,tamanho, sizeof(const char*), compara_strings);
+}
+
 
 
 int main(){
+
+    char **nomes_alunos = NULL;
+    char nome_aluno[20];
+
+    int quantidade_alunos;
+    int aluno_sorteado;
+
+    scanf("%d %d", &quantidade_alunos, &aluno_sorteado);
+
+    nomes_alunos = malloc (quantidade_alunos * sizeof(nome_aluno));
+
+    for(int i=0; i<quantidade_alunos; i++){
+        char* nome_aluno = (char*) malloc (sizeof(char)*20);
+        scanf("%s", nome_aluno);
+        nomes_alunos[i] = nome_aluno;
+    }
+
+    ordena_nome(nomes_alunos, quantidade_alunos);
+
+    for(int i=0; i<quantidade_alunos; i++){
+        if(i == aluno_sorteado-1){
+           printf("%s\n", nomes_alunos[i]);
+        }
+    }
 
     return 0;
 }
